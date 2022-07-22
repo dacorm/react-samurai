77099eed0bf7828967b94ebf547e5b67618934c4
Utkin Denis <stolen.youth@yandex.ru>
2022-07-23 03:13:10 +0700

    feat: add container components

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,52 @@
-import React from 'react';
-import s from "../Users/Users.module.css";
-import axios from 'axios';
-import userIcon from './png-clipart-businessperson-computer-icons-avatar-avatar-heroes-public-relations.png'
+import s from "./Users.module.css";
+import userIcon from "./png-clipart-businessperson-computer-icons-avatar-avatar-heroes-public-relations.png";
+import React from "react";
+
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then( response => {
-            props.setUsers(response.data.items)
-        })
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= 8; i++) {
+        pages.push(i)
     }
-    const users = props.users.map(u =>
-        (
-        <div className={s.wrapper}>
-            <div className={s.avContainer}>
-                <div className={s.round} style={{ backgroundImage:`url(${u.photos.small != null ? u.photos.small : userIcon})` }} />
-                {
-                    u.followed ? <button type='submit' onClick={() => props.unfollow(u.id)} className={s.button}>Unfollow</button>
-                        : <button type='submit' onClick={() => props.follow(u.id)} className={s.button}>Follow</button>
-                }
-            </div>
-            <div className={s.userInfo}>
-                <div className={s.textWrapper}>
-                    <p className={s.name}>{u.name}</p>
-                    <p className={s.desc}>{u.status}</p>
-                </div>
-                <p className={s.city}>{u?.location?.city != null ? u.location.city : 'Russia'}</p>
-            </div>
-        </div>
-    ))
 
     return (
         <div>
             <h1>Users</h1>
+            <div>
+                {
+                    pages.map(p => <button onClick={(e) => { props.setPage(p) }} className={props.currentPage === p ? `${s.pagBtn} + ${s.pagBtnActive}` : s.pagBtn} key={p}>{p}</button>)
+                }
+            </div>
             {
-                users
+                props.users.map(u =>
+                    (
+                        <div className={s.wrapper} key={u.id}>
+                            <div className={s.avContainer}>
+                                <div className={s.round}
+                                     style={{backgroundImage: `url(${u.photos.small != null ? u.photos.small : userIcon})`}}/>
+                                {
+                                    u.followed ? <button type='submit' onClick={() => props.unfollow(u.id)}
+                                                         className={s.button}>Unfollow</button>
+                                        : <button type='submit' onClick={() => props.follow(u.id)}
+                                                  className={s.button}>Follow</button>
+                                }
+                            </div>
+                            <div className={s.userInfo}>
+                                <div className={s.textWrapper}>
+                                    <p className={s.name}>{u.name}</p>
+                                    <p className={s.desc}>{u.status}</p>
+                                </div>
+                                <p className={s.city}>{u?.location?.city != null ? u.location.city : 'Russia'}</p>
+                            </div>
+                        </div>
+                    ))
             }
             <button type='submit' className={s.buttonShow}>Show More</button>
         </div>
     )
 }
 
-export default Users
+export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
-import Users from "./UsersC";
+import Users from "./Users";
 import {connect} from "react-redux";
 import {followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC} from "../../redux/users-reducer";
+import axios from 'axios';
+
+
+
+
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            })
+        }
+    }
+
+    setPage = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        });
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      setPage={this.setPage}
+                      users={this.props.users}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}
+        />
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -32,4 +67,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
